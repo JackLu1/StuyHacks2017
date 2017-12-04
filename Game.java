97b5d381154f2346d2b242741f987debf5cab4f7
Timothy So <so.timothy51@gmail.com>
2017-12-04 17:11:30 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Game{
 
 	public static void main(String[] args){
-		Pokemon p0 = new Pokemon( "Pikachu", 10, 11, 12, 13, 14, 15);
-		System.out.println(p0);
+		Pokemon Pikachu = new Pokemon( "Pikachu", 10, 11, 12, 13, 14, 15);
+		System.out.println(Pikachu);
 	}
 
 	public void newGame(){
